ff794c21dfe415fdec314e93f6e4c9b6ab83cc28
Jiseong <intersian@naver.com>
2022-12-04 23:13:46 +0900

    img 경로 수정

--- js/background.js
+++ js/background.js
@@ -39,6 +39,6 @@ const images = [
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url('img/${chosenImage}')`;
+document.body.style.backgroundImage = `url('./img/${chosenImage}')`;
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
